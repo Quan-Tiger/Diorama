cf1afdad88a44a99aa5adf3b10c5227b17184002
joness1992 <joness1992@hotmail.co.uk>
2025-03-19 20:46:15 +0000

    Basic SKSE Plugin creation

---
+++ cmake/headerlist.cmake
@@ -0,0 +1,8 @@
+set(headers ${headers}
+	include/PCH.h 
+    include/Plugin.h
+    include/Lib/Logger.h
+    src/util.h
+    src/hook.h 
+    src/settings.h
+)
---
+++ src/plugin.cpp
@@ -0,0 +1,40 @@
+#include <Plugin.h>
+
+
+void OnDataLoaded()
+{
+   
+}
+
+void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
+{
+	switch (a_msg->type) {
+	case SKSE::MessagingInterface::kDataLoaded:
+        
+		break;
+	case SKSE::MessagingInterface::kPostLoad:
+		break;
+	case SKSE::MessagingInterface::kPreLoadGame:
+		break;
+	case SKSE::MessagingInterface::kPostLoadGame:
+        break;
+	case SKSE::MessagingInterface::kNewGame:
+		break;
+	}
+}
+
+SKSEPluginLoad(const SKSE::LoadInterface *skse) {
+    SKSE::Init(skse);
+	SetupLog();
+
+	logger::info("Plugin loaded");
+	logger::info("init");
+
+    auto messaging = SKSE::GetMessagingInterface();
+	if (!messaging->RegisterListener("SKSE", MessageHandler)) {
+		return false;
+	}
+
+	
+    return true;
+}
